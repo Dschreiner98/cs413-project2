6e3b611cb26479202862103873111a963109f89a
Your Name <djs554@nau.edu>
2019-10-07 23:45:20 -0700

    final sound fixes

--- game.js
+++ game.js
@@ -1,4 +1,5 @@
 var gameport = document.getElementById("gameport");
+PIXI.Loader.shared.add("spriteSheet.json").load(setup);
 
 var renderer = PIXI.autoDetectRenderer({width: 400, height: 400, backgroundColor: 0x444444});
 gameport.appendChild(renderer.view);
@@ -11,8 +12,8 @@ var score = 0;
 
 var time = 30;
 
-//PIXI.sound.add('dead', 'dead.wav');
-//PIXI.sound.add('win', 'Win.wav');
+PIXI.sound.add('dead', 'dead.wav');
+PIXI.sound.add('win', 'Win.wav');
 
 //Game over text
 gameOver = new PIXI.Text("GAME OVER \nThe Ghost hunters\ngot you!");
@@ -32,7 +33,7 @@ restart.interactive = true;
 restart.click = function(e)
 {
 	location.reload();
-};
+}
 
 //Welcome screen
 welcome = new PIXI.Text("Welcome the the Haunting Game!\n         Press space to begin!");
@@ -40,10 +41,45 @@ welcome.x = 5;
 welcome.y = 100;
 stage.addChild(welcome)
 
+//credits info
+info = new PIXI.Text("Game and sound by Dylan");
+info.x = 35;
+info.y = 100;
 
-var playerTexture = PIXI.Texture.from("Ghost.png");
+//back
+back = new PIXI.Text("Back");
+back.x = 160;
+back.y = 200;
+back.interactive = true;
+back.click = function(e)
+{
+  stage.removeChild(back);
+  stage.removeChild(info);
+  stage.addChild(welcome);
+  stage.addChild(credits);
+}
+//Credits
+credits = new PIXI.Text("Credits");
+credits.x = 145;
+credits.y = 300;
+credits.interactive = true;
+credits.click = function(e)
+{
+  stage.removeChild(welcome);
+  stage.removeChild(credits);
+  stage.addChild(info);
+  stage.addChild(back);
+}
+stage.addChild(credits);
 
-var player = new PIXI.Sprite(playerTexture);
+
+function setup()
+{
+var sheet = PIXI.Loader.shared.resources["spriteSheet.json"].spritesheet;
+
+
+
+var player = new PIXI.Sprite(sheet.textures["Ghost.png"]);
 
 //Initial placement of players character
 player.anchor.x = 0.5;
@@ -57,18 +93,18 @@ player.scale.y = .3;
 
 
 //House creation for game
-var houseTexture = PIXI.Texture.from("House.png");
+
 
 var houses = [];
-var house = new PIXI.Sprite(houseTexture);
+var house = new PIXI.Sprite(sheet.textures["House.png"]);
 houses.push(house);
-var house1 = new PIXI.Sprite(houseTexture);
+var house1 = new PIXI.Sprite(sheet.textures["House.png"]);
 houses.push(house1);
-var house2 = new PIXI.Sprite(houseTexture);
+var house2 = new PIXI.Sprite(sheet.textures["House.png"]);
 houses.push(house2);
-var house3 = new PIXI.Sprite(houseTexture);
+var house3 = new PIXI.Sprite(sheet.textures["House.png"]);
 houses.push(house3);
-var house4 = new PIXI.Sprite(houseTexture);
+var house4 = new PIXI.Sprite(sheet.textures["House.png"]);
 houses.push(house4);
 
 house.position.x = 350;
@@ -91,43 +127,45 @@ house4.position.x = 50;
 house4.position.y = 150;
 
 
-//Light creation for game
-var lightTexture = PIXI.Texture.from("Light.png");
-
-var light = new PIXI.Sprite(lightTexture);
-
 
 
 //Used for keyboard input
 function keydownEventHandler(e) {
 
     if (e.keyCode == 87) { // W key
+      PIXI.sound.play('dead');
       var newy = player.position.y - 40;
       createjs.Tween.get(player.position).to({x: player.position.x, y: newy}, 500);
     }
   
     if (e.keyCode == 83) { // S key
+      PIXI.sound.play('dead');
       var newy = player.position.y + 40;
       createjs.Tween.get(player.position).to({x: player.position.x, y: newy}, 500);
     }
     if (e.keyCode == 65) { // A key
+      PIXI.sound.play('dead');
       var newx = player.position.x - 40;
       createjs.Tween.get(player.position).to({x: newx, y: player.position.y}, 500);
     }
   
     if (e.keyCode == 68) { // D key
+      PIXI.sound.play('dead');
       var newx = player.position.x + 40;
       createjs.Tween.get(player.position).to({x: newx, y: player.position.y}, 500);
     }
     
     if (e.keyCode == 32) {
+      PIXI.sound.play('win');
+      
       stage.addChild(player);
       stage.addChild(house);
       stage.addChild(house1);
       stage.addChild(house2);
       stage.addChild(house3);
       stage.addChild(house4);
-      stage.removeChild(welcome)
+      stage.removeChild(welcome);
+      stage.removeChild(credits);
     }
   }
 
@@ -145,13 +183,13 @@ function animate()
         var ghostBounds = player.getBounds();
         
         if((houseCoordinates.x + houseCoordinates.width/2) + houseCoordinates.width > (ghostBounds.x + ghostBounds.width/2) 
-        && (houseCoordinates.x + houseCoordinates.width/2) < (ghostBounds.x+ ghostBounds.width/2) + playerTexture.width 
+        && (houseCoordinates.x + houseCoordinates.width/2) < (ghostBounds.x+ ghostBounds.width/2) + player.width 
         && (houseCoordinates.y +houseCoordinates.height/2) + houseCoordinates.height > (ghostBounds.y + ghostBounds.height/2) 
         && (houseCoordinates.y+houseCoordinates.height/2) < (ghostBounds.y + ghostBounds.height/2) + ghostBounds.height)
         {
             if(i != rand)
             {
-              PIXI.sound.play('dead');
+              
                 stage.addChild(gameOver);
                 stage.addChild(restart);
                 
@@ -165,7 +203,7 @@ function animate()
         }
         if(score>1)
         {
-            PIXI.sound.play('win');
+            
             stage.addChild(youWin);
             stage.addChild(restart);
             
@@ -180,5 +218,6 @@ function animate()
     }
 
 }
-
 animate();
+}
+
